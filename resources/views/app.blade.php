3605a9e0119dc69fcd930169cfd1066e80bcb787
SuwTaisha <hoangmen0715@gmail.com>
2026-07-08 16:10:23 +0700

    fix loi font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=be-vietnam-pro:400,500,600,700,800" rel="stylesheet" />
 
         @routes
         @viteReactRefresh
